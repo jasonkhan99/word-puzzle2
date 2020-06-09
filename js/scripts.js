2168a301abd58676b46d8a8e3ef23b8bd8efa037
Joseph Pearce <joseph.pearce05@gmail.com>
2020-06-09 11:53:27 -0700

    word puzzle solved
    co-authored-by Jason Khan <jasonderekkhan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,24 @@ $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
     let sentence = $('input#sentence').val();
+    
 
     console.log(sentence);
 
     let sentenceSplit = sentence.split("");
-    let finalSentence = [];
+  
     console.log(sentenceSplit);
-
-    for (let i=0; sentenceSplit.length > i; i+=1) {
-      let finalSentence = sentenceSplit[i];
-      if (finalSentence === "a" || finalSentence === "e" || finalSentence === "i" || FinalSentence === "o" || finalSentence === "u") {
-      finalSentence[i] = '-';
-      } else {
-        finalSentence2[i]=finalSentence;
-        alert ("test ");
+    
+    for (let i=0; sentenceSplit.length > i; ++i) {
+      if (sentenceSplit[i] === 'a' || sentenceSplit[i] === 'e' || sentenceSplit[i] === 'i' || sentenceSplit[i] === 'o' || sentenceSplit[i] === 'u') {
+        sentenceSplit[i] = '-';
       }
+    }
+    
+    let sentenceJoin = sentenceSplit.join('');
+    console.log(sentenceSplit);
+    $('.output').append(('<p>' + sentenceJoin + '<p>'));
+        
 
   });
 });
---
+++ js/scripts2.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("#form").submit(function(event) {
+    event.preventDefault();
+    let sentence = $('input#sentence').val();
+
+    console.log(sentence);
+
+    let sentenceSplit = sentence.split("");
+    let finalSentence = [];
+    console.log(sentenceSplit);
+
+    for (let i=0; sentenceSplit.length > i; i+=1) {
+      let finalSentence = sentenceSplit[i];
+      if (finalSentence === "a" || finalSentence === "e" || finalSentence === "i" || FinalSentence === "o" || finalSentence === "u") {
+      finalSentence[i] = '-';
+      } else {
+        finalSentence2[i]=finalSentence;
+        
+
+  });
+});
